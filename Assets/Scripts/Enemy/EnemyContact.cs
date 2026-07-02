6b4f35ba08bff9578c469c2a595376db854decee
AbiralTr <abiraltuladhar22@gmail.com>
2026-07-01 21:13:15 -0600

    Created a script to handle player health, created a script to handle enemy contact damage, added 3 static enemies with hitboxes into the test scene env

---
+++ Assets/Scripts/Enemy/EnemyContact.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class EnemyContact : MonoBehaviour
+{
+    [SerializeField] private int damage = 10;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerHealth health = collision.gameObject.GetComponent<PlayerHealth>();
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+            }
+        }
+    }
+}
