a87715c271d1cfa968a42c8c96dc846a108e4f6a
Abiral Tuladhar <103455365+AbiralTr@users.noreply.github.com>
2026-07-13 22:31:40 -0600

    Added basic player death + restart with UI (#32)

---
+++ Assets/Scripts/UI/DeathScreenManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreenManager : MonoBehaviour
+{
+    [SerializeField] private GameObject deathScreenUI;
+
+    public void Show()
+    {
+        Time.timeScale = 0f;
+        deathScreenUI.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
